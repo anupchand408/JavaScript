0fad25ae2b2be8bfa3929322a6bc41c76144a82b
Anup Chand <anupchand898@gmail.com>
2024-09-20 15:41:51 +0545

    Initial commit

---
+++ first.js
@@ -0,0 +1,15 @@
+console.log("Lats's start journey in JS ");
+console.log("Anup Chand")
+console.log("First day of js")
+fullName = "Anup Chand";
+age = 22;
+console.log(fullName);
+console.log(age);
+x = null;
+console.log(x);
+y = undefined;
+console.log(y);
+isFollow = false;
+console.log(isFollow);
+let totalPrice = 1000;
+console.log(totalPrice);
